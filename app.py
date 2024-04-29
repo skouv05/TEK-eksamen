4ae6d73c6dbeb073800aef8f347deba67d53459c
skouv05 <jakobskouv@gmail.com>
2024-04-29 12:12:26 +0200

    added db, products page, front page and andmin page

--- app.py
+++ app.py
@@ -1,12 +1,71 @@
-from flask import Flask, request, jsonify, render_template
-
+from flask import Flask, request, jsonify, render_template, redirect
+import mysql.connector
+import os
 
 app = Flask(__name__)
 
+productobjects = []
+
+class product:
+    def __init__(self, id, name, price, description):
+        self.id = id
+        self.name = name
+        self.price = price
+        self.description = description
+        self.image = name+".jpg" 
+
+
+mydb = mysql.connector.connect(
+  host="127.0.0.1",
+  user="root",
+  database="products"
+  
+)
+
+cursor = mydb.cursor()
+cursor.execute("SELECT * FROM `product`")
+records = cursor.fetchall()
+for i in records:
+    productobjects.append(product(i[0], i[1], i[2], i[3]))
+
+
+
 @app.route("/")
 def hello_world():
+
     return render_template('index.html')
 
+@app.route("/products")
+def products():
+    return render_template('products.html', prods=productobjects)
+
+@app.route("/product/<id>")
+def show_product(id):
+    for i in productobjects:
+        if i.name == str(id):
+            return render_template('product.html', prod=i)
+    return "Product not found"
+
+@app.route("/admin", methods=['GET', 'POST'])
+def admin():
     
+    if request.method=="POST":
+       
+        name = request.form['name']
+        price = request.form['price']
+        description = request.form['description']
+       
+        if name=="" or price=="" or description=="":
+            return "Please fill all the fields"
+        cursor.execute(f"INSERT INTO `product` (`name`, `price`, `des`) VALUES ('{name}', '{price}', '{description}')")
+        mydb.commit()
+        productobjects.append(product(productobjects[-1].id+1, name, price, description))
+        return "Produkt tilføjet til database. Tilføj et billede til produktet i mappen 'static' og navngiv det samme som produktet."
+        
+    return render_template('admin.html')
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
